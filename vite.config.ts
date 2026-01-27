087a2f4b5d238f3dc658de9e318a9e6ccc28f422
nwpcgn <119536313+nwpcgn@users.noreply.github.com>
2026-01-27 12:20:20 +0100

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [tailwindcss(), svelte()] })
+export default defineConfig({
+	plugins: [tailwindcss(), svelte()],
+	base: '/fahrdienst-01/'
+})
